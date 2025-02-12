bca84eac74e43e2f25607cbb39d17856c6cc38bb
abrarhamim40 <abrarhamim4044j@gmail.com>
2025-02-12 12:36:23 +0600

    - Create some new Folder - Create Some file

---
+++ GitPractice/GitPractice/Model/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  GitPractice
+//
+//  Created by Beyond_2 on 12/2/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Person {
+    var name: String
+    var age: Int
+    var image: String
+    var gender: String
+    var location: String
+}
